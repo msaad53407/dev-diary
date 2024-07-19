ad4a5b27865de6af25b395a86dc58d318c1ad194
Muhammad Saad <msmuhammadsaad78@gmail.com>
2024-07-19 23:56:25 +0500

    Add featured post section on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import { allPosts } from "contentlayer/generated";
 import PostCard from "@/components/post-card";
+import FeaturedPostCard from "@/components/featured-post-card";
 
 export default function Home() {
-  const publishedPosts = allPosts.filter((post) => post.published);
+  const latestPublishedPosts = allPosts
+    .filter((post) => post.published)
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
-  if (publishedPosts.length === 0) {
+  if (latestPublishedPosts.length === 0) {
     return (
       <main className="w-full h-screen px-4 sm:px-6 lg:px-8 flex items-center justify-center">
         <h1 className="text-3xl font-bold text-text-primary">No Blogs Published</h1>
@@ -14,14 +17,12 @@ export default function Home() {
   }
 
   return (
-    <main className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 space-y-5 py-6">
-      {/**
-       * LatestPost
-       */}
+    <main className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 space-y-20 py-6">
+      <FeaturedPostCard post={latestPublishedPosts[0]} />
       <section className="flex flex-col gap-4">
         <h3 className="text-lg font-bold text-text-primary">Latest Posts</h3>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {publishedPosts.map((post) => (
+          {latestPublishedPosts.map((post) => (
             <PostCard post={post} key={post._id} />
           ))}
         </div>
---
+++ src/components/featured-post-card.tsx
@@ -0,0 +1,38 @@
+import { Post } from "contentlayer/generated";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Card, CardContent, CardFooter } from "./ui/card";
+import { formatDate } from "@/lib/utils";
+
+const FeaturedPostCard = ({ post }: { post: Post }) => {
+  return (
+    <Link
+      href={`/post/${post.slug}`}
+      className="flex max-w-screen-md 2xl:max-w-max 2xl:w-full h-[400px] 2xl:h-[600px] mx-auto relative rounded-xl"
+    >
+      <Image
+        src={post.coverImage}
+        alt="Next.js"
+        width={500}
+        height={500}
+        quality={100}
+        className="w-full object-cover rounded-xl"
+      />
+      <Card className="w-[350px] absolute -bottom-14 left-8">
+        <CardContent className="flex flex-col gap-3 p-6">
+          <span className="px-2 py-1 rounded-xl bg-theme-primary-light w-fit text-theme-primary text-sm capitalize">
+            {post.category}
+          </span>
+          <h3 className="text-xl font-bold text-text-primary capitalize">{post.title}</h3>
+        </CardContent>
+        <CardFooter className="flex justify-between p-6">
+          <p className="text-sm text-secondary-400">Muhammad Saad</p>
+          <p className="text-sm text-secondary-400">{formatDate(post.date)}</p>
+        </CardFooter>
+      </Card>
+    </Link>
+  );
+};
+
+export default FeaturedPostCard;
