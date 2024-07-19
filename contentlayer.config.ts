c15aaf5c9e3c62e30e87f54f34369471d3e78679
Muhammad Saad <msmuhammadsaad78@gmail.com>
2024-07-20 00:50:07 +0500

    Add plugings to contentlayer to beautify mdx output

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,8 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
 
 export const Post = defineDocumentType(() => ({
   name: "Post",
@@ -30,4 +34,39 @@ export default makeSource({
   contentDirPath: "src/content/blogs",
   documentTypes: [Post],
   disableImportAliasWarning: true,
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        //@ts-ignore
+        rehypePrettyCode,
+        {
+          theme: "github-dark",
+          onVisitLine(node: any) {
+            // Prevent lines from collapsing in `display: grid` mode, and allow empty
+            // lines to be copy/pasted
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }];
+            }
+          },
+          onVisitHighlightedLine(node: any) {
+            node.properties.className.push("line--highlighted");
+          },
+          onVisitHighlightedWord(node: any) {
+            node.properties.className = ["word--highlighted"];
+          },
+        },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section",
+          },
+        },
+      ],
+    ],
+  },
 });
